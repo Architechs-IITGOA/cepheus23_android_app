5928e9223325cb35fd3c012bb6ea423624318e93
ifeelcodes <vivekbandrele11@gmail.com>
2022-10-26 19:34:04 +0530

    Task: change the xml files accordingly

---
+++ app/src/main/java/com/example/cepheus23/Homescreen.kt
@@ -0,0 +1,35 @@
+package com.example.cepheus23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.cepheus23.databinding.HomelayoutBinding
+import com.google.android.material.navigation.NavigationView
+
+class Homescreen : AppCompatActivity() {
+    private lateinit var binding: HomelayoutBinding
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HomelayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawerLayout:DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
