d57f4f39a94aaa10490cd625cbb4b4240ac6871a
ifeelcodes <vivekbandrele11@gmail.com>
2022-10-30 16:01:31 +0530

    Implemented bottom navigation drawer, updated navigation drawer

--- app/src/main/java/com/example/cepheus23/Homescreen.kt
+++ app/src/main/java/com/example/cepheus23/Homescreen.kt
@@ -1,10 +1,14 @@
 package com.example.cepheus23
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.cepheus23.databinding.HomelayoutBinding
 import com.google.android.material.navigation.NavigationView
 
@@ -12,6 +16,8 @@ class Homescreen : AppCompatActivity() {
     private lateinit var binding: HomelayoutBinding
 
     lateinit var toggle: ActionBarDrawerToggle
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,28 @@ class Homescreen : AppCompatActivity() {
         val navView: NavigationView = binding.navView
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavView
+        setupWithNavController(bottomNavigationView,navController)
+
+
+
+
+
+
+
+
+
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
